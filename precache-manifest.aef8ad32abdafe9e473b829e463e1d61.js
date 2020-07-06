e10fb284c8c0f1e640f71e4cb642bd40dcde700c
JOHN <xiaomozi_2016@outlook.com>
2020-07-06 09:28:26 +0800

    init
    Signed-off-by: JOHN <xiaomozi_2016@outlook.com>

---
+++ precache-manifest.aef8ad32abdafe9e473b829e463e1d61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf3c3b39dc3d58eba937e7e77efcdb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79eccd08b56d74db6c7a",
+    "url": "/static/css/2.2c62aa31.chunk.css"
+  },
+  {
+    "revision": "74b3e24558037db441e7",
+    "url": "/static/css/main.1e5537a8.chunk.css"
+  },
+  {
+    "revision": "79eccd08b56d74db6c7a",
+    "url": "/static/js/2.6f617d0f.chunk.js"
+  },
+  {
+    "revision": "b88bca802d90b4a3030e9a05cb0ffaf7",
+    "url": "/static/js/2.6f617d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b3e24558037db441e7",
+    "url": "/static/js/main.5b82e92b.chunk.js"
+  },
+  {
+    "revision": "367dd8577bcf7355b67e",
+    "url": "/static/js/runtime-main.69a009c2.js"
+  },
+  {
+    "revision": "a9c243255cbc27baff18699e6a665f04",
+    "url": "/static/media/heatmap-data.a9c24325.csv"
+  }
+]);
